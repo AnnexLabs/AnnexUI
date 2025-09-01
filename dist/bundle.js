4c1bbc20acb27c4f69d80bc383a4799a53147674
Oliver Nassar <onassar@gmail.com>
2025-09-01 01:17:45 -0400

    [AUTOMATED] Dist build process

--- dist/bundle.js
+++ dist/bundle.js
@@ -373,9 +373,9 @@ window.annexSearch.DependencyLoader.push([], function() {
          * 
          * @access  private
          * @static
-         * @var     String (default: '0.1.9-release')
+         * @var     String (default: '0.1.11-stable')
          */
-        static #__version = '0.1.9-release';
+        static #__version = '0.1.11-stable';
 
         /**
          * #__setupUtils
@@ -1123,7 +1123,7 @@ window.annexSearch.DependencyLoader.push(['window.annexSearch.BaseHelper'], func
              */
             resources: {
                 css: [
-                    'https://cdn.jsdelivr.net/gh/AnnexLabs/AnnexUI@0.1.9-release/dist/bundle.min.css',
+                    'https://cdn.jsdelivr.net/gh/AnnexLabs/AnnexUI@0.1.11-stable/dist/bundle.min.css',
                 ],
             },
 
